c4942442b21a607f2bf30c4c5f3a9cdd9c505414
scastrox <scastrox87@gmail.com>
2026-08-14 12:01:07 -0600

    feat: implement Popup component with dynamic content and close functionality in Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,24 @@
-function Main() {
+import { useState } from 'react';
+import Popup from './components/Popup/Popup.jsx';
+import NewCard from './components/Popup/form/NewCard/NewCard.jsx';
+import EditProfile from './components/Popup/form/EditProfile/EditProfile.jsx';
+import EditAvatar from './components/Popup/form/EditAvatar/EditAvatar.jsx';
+
+const newCardPopup = { title: 'Nuevo Lugar', children: <NewCard /> };
+const editProfilePopup = { title: 'Editar perfil', children: <EditProfile /> };
+const editAvatarPopup = { title: 'Cambiar foto de perfil', children: <EditAvatar /> };
+
+export default function Main() {
+  const [popup, setPopup] = useState(null);
+
+  function handleOpenPopup(popupToOpen) {
+    setPopup(popupToOpen);
+  }
+
+  function handleClosePopup() {
+    setPopup(null);
+  }
+
   return (
     <main className="content">
       <section className="profile">
@@ -6,23 +26,42 @@ function Main() {
           <div className="profile__content">
             <div className="profile__avatar">
               <img className="profile__picture" alt="Foto de perfil del usuario" />
-              <button className="profile__avatar-edit-button" type="button"></button>
+              <button
+                aria-label="Cambiar foto de perfil"
+                className="profile__avatar-edit-button"
+                type="button"
+                onClick={() => handleOpenPopup(editAvatarPopup)}
+              />
             </div>
             <div className="profile__info">
               <h1 className="profile__info-name">Sergio Castro</h1>
               <p className="profile__info-description">Estudiante</p>
             </div>
-            <button className="profile__info-edit-button" type="button"></button>
+            <button
+              aria-label="Editar perfil"
+              className="profile__info-edit-button"
+              type="button"
+              onClick={() => handleOpenPopup(editProfilePopup)}
+            />
           </div>
         </div>
         <div>
-          <button className="profile__info-add-button" type="button"></button>
+          <button
+            aria-label="Agregar tarjeta"
+            className="profile__info-add-button"
+            type="button"
+            onClick={() => handleOpenPopup(newCardPopup)}
+          />
         </div>
       </section>
 
       <section className="gallery">{/* Las tarjetas se generarán dinámicamente con React */}</section>
+
+      {popup && (
+        <Popup onClose={handleClosePopup} title={popup.title}>
+          {popup.children}
+        </Popup>
+      )}
     </main>
   );
 }
-
-export default Main;
--- src/components/Main/components/Popup/Popup.jsx
+++ src/components/Main/components/Popup/Popup.jsx
@@ -1,11 +1,16 @@
 export default function Popup(props) {
   // los hijos son el contenido de la ventana emergente
-  const { title, children } = props;
+  const { title, children, onClose } = props;
 
   return (
     <div className="popup">
       <div className="popup__container">
-        <button aria-label="Cerrar ventana emergente" className="popup__close-button" type="button" />
+        <button
+          aria-label="Cerrar ventana emergente"
+          className="popup__close-button"
+          type="button"
+          onClick={onClose}
+        />
         <h2 className="popup__form-title">{title}</h2>
         {children}
       </div>
